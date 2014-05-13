705f10046f856132a25d188814c549b243e79810
Maxim <m@1435.ru>
2014-05-13 13:19:31 +0600

    Main Controller Preparing
    Main Controller preparing, added basic functions

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,9 +1,23 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+//Main Controller. Global Initializing
+
 class Main extends CI_Controller {
 
-	public function index()
+	public function __construct()
 	{
-		$this->load->view('main');
+		parent::__construct();
+		//TODO Application interface language
+		//TODO Environment props
+		$this->load->model('modMain');
+		
+		//Load Template Parser library
+		//$this->load->library('parser');
+		
+	}
+       
+	public function index()
+	{	$params['params'] = $this->modMain->getTemplate();
+		$this->load->view('template', $params);
 	}
 }
